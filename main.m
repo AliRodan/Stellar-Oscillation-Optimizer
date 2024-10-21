8285967a701f1cc7035dd9d5cc1b4e9560cde174
AliRodan <alirodan@gmail.com>
2024-10-21 14:16:40 +0300

    main.m

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %______________________________________________________________________________________________%
-%  Stellar Oscillation Optimizer (SOO) source codes (version 1.0)                              %
+%  Stellar Oscillation Optimizer (SOO) source codes (version 1.1)                              %
 %                                                                                              %
 %  Developed in MATLAB R2018a (7.13)                                                           %
 %  Author and programmer: Ali Rodan                                                            %
@@ -8,27 +8,26 @@
 %                         1- https://scholar.google.co.uk/citations?user=n8Z3RMwAAAAJ&hl=en    %
 %                         2- https://www.researchgate.net/profile/Ali-Rodan                    %
 %                                                                                              %
-%   Paper Title:Stellar Oscillation Optimizer: A novel meta-heuristic optimization algorithm   %
-%                   A. Rodan, A. Al-Tamimi, L. Alnemer (2024).                                 %
-%          Stellar Oscillation Optimizer: A novel meta-heuristic optimization algorithm.       %
+%   Paper Title:Stellar Oscillation Optimizer: A Nature-inspired metaheuristic optimization algorithm.   %
+%                   A. Rodan, A. Al-Tamimi, L. Alnemer, S. Mirjalili (2024)                                  %
+%          Stellar Oscillation Optimizer: A Nature-inspired metaheuristic optimization algorithm.       %
 %                                                                                 %
 %______________________________________________________________________________________________%
 
 clear all 
 clc
-
-Oscillator_no=30;
+% Define your parameters and objective function
+StarOsc_no=50;
 f_name='F1';
-m_iter=1000; 
+m_iter=500; 
+
+%The Classical 23 Benchmark Functions
+
+% Call the objective function with the defined parameters
+[lb,ub,dim,fobj]=Get_F(f_name);
 
 
-[lb,ub,dim,f_obj]=Get_F(f_name);
+% Call the optimizer
+[best_luminosity, best_phase_position, luminosity_curve] = SOO(StarOsc_no, m_iter, lb, ub, dim, fobj);
 
-[b1_best,b1_L]=SOO(Oscillator_no,m_iter,lb,ub,dim,f_obj);
-
-display(['The best optimal value of the objective funciton found by SOO is : ', num2str(b1_best)]);
-
-        
-
-
-
+display(['The best optimal value of the objective funciton found by SOO is : ', num2str(best_luminosity)]);
